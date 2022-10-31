442c876b97cc775173b35fff2e80821a890a3c00
adamahmadkhan <adamkhan8757@gmail.com>
2022-10-31 11:46:47 +0500

    third

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -10,6 +10,9 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   String mod="";
+  String username="";
+  String password="";
+  String loginornot="";
   final List<String> genderItems = [
     'Admin',
     'Teacher',
@@ -18,129 +21,175 @@ class _LoginScreenState extends State<LoginScreen> {
   ];
   Widget build(BuildContext context) {
     return Scaffold(
-      //hey ??
-        body: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 10,
+
+        body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage("https://images.pexels.com/photos/1762851/pexels-photo-1762851.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+                fit: BoxFit.cover,
+              )
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage('assets/pro.jpg'),
-              ),
-              Text(
-                "Welcome back "+mod,
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    //border: OutlineInputBorder(),
-                    labelText: "Email",
-                    hintText: "example@example.com",
-                    hintStyle: TextStyle(color: Colors.grey.withOpacity(.5)),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.tealAccent,width: 2),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.redAccent,width: 2),
-                      borderRadius: BorderRadius.circular(16),
-                    ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage('assets/pro.jpg'),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextFormField(
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  decoration: InputDecoration(
-                    //border: OutlineInputBorder(),
-                    labelText: "Password",
-                    hintText: "Enter Password",
-                    hintStyle: TextStyle(color: Colors.grey.withOpacity(.5)),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.tealAccent,width: 2),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.redAccent,width: 2),
-                      borderRadius: BorderRadius.circular(16),
-                    ),
+                Text(
+                  "Welcome back "+mod,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: DropdownButtonFormField2(
-                  decoration: InputDecoration(
-                    isDense: true,
-                    contentPadding: EdgeInsets.zero,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      //border: OutlineInputBorder(),
+                      labelText: "Email",
+                      hintText: "example@example.com",
+                      hintStyle: TextStyle(color: Colors.white.withOpacity(.7)),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.tealAccent,width: 2),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.redAccent,width: 2),
+                        borderRadius: BorderRadius.circular(16),
+                      ),
                     ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.tealAccent,width: 2),
-                      borderRadius: BorderRadius.circular(10),
+                    onChanged: (value){
+                      username=value.toString();
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextFormField(
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    decoration: InputDecoration(
+                      //border: OutlineInputBorder(),
+                      labelText: "Password",
+                      hintText: "Enter Password",
+                      hintStyle: TextStyle(color: Colors.white.withOpacity(.7)),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.tealAccent,width: 2),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.redAccent,width: 2),
+                        borderRadius: BorderRadius.circular(16),
+                      ),
                     ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.redAccent,width: 2),
-                      borderRadius: BorderRadius.circular(16),
+                    onChanged: (value){
+                      password=value.toString();
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: DropdownButtonFormField2(
+                    decoration: InputDecoration(
+                      isDense: true,
+                      contentPadding: EdgeInsets.zero,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.tealAccent,width: 2),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.redAccent,width: 2),
+                        borderRadius: BorderRadius.circular(16),
+                      ),
                     ),
-                  ),
-                  isExpanded: true,
-                  hint: const Text(
-                    'Your Are?',
-                    style: TextStyle(fontSize: 14),
-                  ),
-                  icon: const Icon(
-                    Icons.arrow_drop_down,
-                    color: Colors.black45,
-                  ),
-                  iconSize: 30,
-                  buttonHeight: 60,
-                  buttonPadding: const EdgeInsets.only(left: 20, right: 10),
-                  dropdownDecoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  items: genderItems
-                      .map((item) =>
-                      DropdownMenuItem<String>(
-                        value: item,
-                        child: Text(
-                          item,
-                          style: const TextStyle(
-                            fontSize: 14,
+                    isExpanded: true,
+                    hint: const Text(
+                      'Your Are?',
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    icon: const Icon(
+                      Icons.arrow_drop_down,
+                      color: Colors.black45,
+                    ),
+                    iconSize: 30,
+                    buttonHeight: 60,
+                    buttonPadding: const EdgeInsets.only(left: 20, right: 10),
+                    dropdownDecoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    items: genderItems
+                        .map((item) =>
+                        DropdownMenuItem<String>(
+                          value: item,
+                          child: Text(
+                            item,
+                            style: const TextStyle(
+                              fontSize: 14,
+                            ),
                           ),
-                        ),
-                      ))
-                      .toList(),
-                  validator: (value) {
-                    if (value == null) {
-                      return 'Please select modelue';
-                    }
-                  },
-                  onChanged: (value) {
-                    mod = value.toString();
-                    setState(() {
-                    });
-                  },
+                        ))
+                        .toList(),
+                    validator: (value) {
+                      if (value == null) {
+                        return 'Please select modelue';
+                      }
+                    },
+                    onChanged: (value) {
+                      mod = value.toString();
+                      setState(() {
+                      });
+                    },
+                  ),
                 ),
-              ),
-            ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(right: 25),
+                      height: 60,
+                      width: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.cyanAccent,
+                        shape: BoxShape.circle,
+                      ),
+                      child: TextButton(
+                        child: Text("l"),
+                        onPressed: (){
+                          loginuser();
+                          setState(() {
+                          });
+                        },
+                      )
+                    ),
+                  ],
+                ),
+                Text(loginornot),
+              ],
+            ),
           ),
         ),
     );
   }
+  void loginuser(){
+    if(username=="std"&& password=="pwd" && mod=="Student" || username=="tea"&& password=="pwd" && mod=="Teacher"||username=="adm"&& password=="pwd" && mod=="Admin")
+      loginornot= "Yes";
+    else
+      loginornot="invalid password";
+  }
 }
+
